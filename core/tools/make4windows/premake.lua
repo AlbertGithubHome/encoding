563d6b240eef2e2ec8d1f48d891d0bb4ce3b1446
albert <alberts101@163.com>
2017-03-16 12:12:08 +0800

    add project windows compiler batchfile

---
+++ core/tools/make4windows/premake.lua
@@ -0,0 +1,52 @@
+solution "encoding"
+  configurations { "Debug", "Release" } 
+  location "../../code"
+
+  includedirs {
+    "../../code/share",
+    "../../code/share/detection",
+    "../../code/detection",
+  }
+
+  defines{"_DEBUG","_SERVER","WIN32","_WINDOWS","_LIB","MY_CMD","_STANDALONE","_OPENALLFUNCTION","NO_DIRECT_X","_USE_32BIT_TIME_T"}
+  language "C++"
+
+  project "main"
+    location "../../code/main"
+    kind "ConsoleApp"
+    files { "../../code/main/**.h", "../../code/main/**.cpp"}
+    links {"detection"}
+    libdirs{"../../code/share/library/debug"}
+    targetdir "../../bin"
+    debugdir "../../bin"
+      configuration "Debug"
+        defines { "DEBUG" }
+        flags { "Symbols" }
+      configuration "Release"
+        defines { "NDEBUG" }
+        flags { "Optimize" }
+
+  project "detection"
+    location "../../code/detection"
+    kind "StaticLib"
+    files { "../../code/detection/**.h", "../../code/detection/**.cpp"}
+    targetdir "../../code/share/library/debug"
+    targetname "detection"
+      configuration "Debug"
+        flags { "Symbols","FatalWarnings" }
+      configuration "Release"
+        defines { "NDEBUG" }
+        flags { "Optimize", "FatalWarnings"}
+
+  project "share"
+    location "../../code/share"
+    kind "StaticLib"
+    files { "../../code/share/**.h"}
+    targetdir "../../code/share/library/debug"
+    targetname "share"
+      configuration "Debug"
+        flags { "Symbols" }
+      configuration "Release"
+        defines { "NDEBUG" }
+        flags { "Optimize" }
+
---
+++ core/tools/make4windows/readme.txt
@@ -0,0 +1,5 @@
+1.run make.bat to generate project solution
+2.run makeclean.bat to delete project solution
+
+premake.lua is the configuration file.
+see https://github.com/AlbertGithubHome/encoding
